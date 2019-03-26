f929635631b08616cb4812068fd4104eb1eaef52
csabe812 <csafulop@gmail.com>
2019-03-26 20:46:30 +0100

    Initialized a spring boot application

---
+++ src/main/java/com/agrovetel/AgrovetelApplication.java
@@ -0,0 +1,13 @@
+package com.agrovetel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgrovetelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgrovetelApplication.class, args);
+	}
+
+}
