cb878f10e0f75974fda86d8599e8bbce90f2c90f
csabe812 <csafulop@gmail.com>
2019-04-01 21:01:45 +0200

    Started to  implement #1

--- src/main/java/com/agrovetel/config/SecurityConfiguration.java
+++ src/main/java/com/agrovetel/config/SecurityConfiguration.java
@@ -15,14 +15,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Autowired
 	public void configureAuth(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication()
-			.withUser("admin").password("{noop}admin").roles("ADMIN");
+			.withUser("admin@admin.com").password("{noop}admin").roles("ADMIN");
 	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/css/**", "/js/**", "/images/**", "/fonts/**", "/plugins/**").permitAll();
-		/*http
-		.authorizeRequests().anyRequest().authenticated();
+		http
+		/*.authorizeRequests().anyRequest().authenticated();
 			.antMatchers("/tesztoldal").permitAll()
 			.antMatchers("/admin/**").hasRole("ADMIN")
 			.antMatchers("/shop/**").hasRole("USER")
@@ -32,14 +32,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 			.antMatchers("/console/**").permitAll()
 			.antMatchers("/activation/**").permitAll()
 			.anyRequest().authenticated()
-			.and()
+			.and()*/
 			.formLogin()
 			.loginPage("/login")
 			.permitAll()
 			.and()
 			.logout()
 			.logoutSuccessUrl("/login?logout")
-			.permitAll();*/
+			.permitAll();
 		
 		//This is for only H2 embedded database
 		//TODO: comment this if you connect to the postresql
--- src/main/java/com/agrovetel/config/WebConfig.java
+++ src/main/java/com/agrovetel/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addViewController("/login").setViewName("auth/login");
+		registry.addViewController("/login").setViewName("index");;
 		registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
 	}
 	
