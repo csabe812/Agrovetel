aa9c18db299929a311c4a21bddfd17efa1842f5c
csabe812 <csafulop@gmail.com>
2019-04-04 23:09:02 +0200

    FEAT|#NOID|added new test data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,13 @@
 insert into ads (title, price, horse_power, sell_or_search, year_of_manufact)
 values ('Ad1', 20, 150, true, 1980);
+insert into ads (title, price, horse_power, sell_or_search, year_of_manufact)
+values ('Ad2', 30, 3, false, 2019);
+insert into ads (title, price, horse_power, sell_or_search, year_of_manufact)
+values ('Ad2', 220, 1150, true, 1994);
 
 insert into users(email, enabled, password, fullname)
-values('example@example.com', true, 'password', 'Joe Doe');
+values('joe@doe.com', true, 'joepass', 'Joe Doe');
+insert into users(email, enabled, password, fullname)
+values('jane@doe.com', false, 'janepass', 'Jane Doe');
+insert into users(email, enabled, password, fullname)
+values('king@kong.com', true, 'kingkongpass', 'King Kong');
