f9c68e3b31fb6129923056a15d574f07763fe81c
csabe812 <csafulop@gmail.com>
2019-04-11 23:08:51 +0200

    FEAT|#13|Editing user (not completed)

--- src/main/java/com/agrovetel/controller/AdminController.java
+++ src/main/java/com/agrovetel/controller/AdminController.java
@@ -1,5 +1,7 @@
 package com.agrovetel.controller;
 
+import javax.validation.Valid;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.agrovetel.domain.User;
 import com.agrovetel.service.UserService;
 
 @Controller
@@ -17,9 +20,14 @@ import com.agrovetel.service.UserService;
 public class AdminController {
 
 	private final Logger log = LoggerFactory.getLogger(this.getClass());
-	
+
 	private UserService userService;
-	
+
+	/**
+	 * Setter-based autowiring
+	 * 
+	 * @param userService
+	 */
 	@Autowired
 	public void setUserService(UserService userService) {
 		this.userService = userService;
@@ -30,14 +38,39 @@ public class AdminController {
 		log.debug("Loading admin site");
 		return "admin";
 	}
-	
+
+	/**
+	 * List of the users
+	 * 
+	 * @param model
+	 * @return
+	 */
 	@GetMapping("/users")
 	public String showUsers(Model model) {
 		log.info("Getting users");
 		model.addAttribute("users", this.userService.findAll());
 		return "users";
 	}
+
+	@GetMapping("/users/{id}")
+	public String editUser(@PathVariable long id, Model model) {
+		log.info("Getting a user by id");
+		model.addAttribute("user", this.userService.findById(id));
+		return "user";
+	}
 	
+	@GetMapping("/users/{id}/update")
+	public String updateUser(@PathVariable long id, @Valid User updatedUser) {
+		log.info("Updating user");
+		this.userService.updateUser(id, updatedUser);
+		return "users";
+	}
+	
+	/**
+	 * Deletes a user
+	 * @param id
+	 * @return
+	 */
 	@GetMapping("/users/{id}/delete")
 	public String deleteUser(@PathVariable long id) {
 		log.info("started");
--- src/main/java/com/agrovetel/service/UserService.java
+++ src/main/java/com/agrovetel/service/UserService.java
@@ -2,6 +2,8 @@ package com.agrovetel.service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +12,7 @@ import com.agrovetel.repository.UserRepository;
 
 @Service
 public class UserService {
+	private final Logger log = LoggerFactory.getLogger(this.getClass());
 
 	private UserRepository userRepository;
 
@@ -22,8 +25,34 @@ public class UserService {
 		return this.userRepository.findAll();
 	}
 	
+	/**
+	 * Finding a user by its id
+	 * @param id user id
+	 * @return found user
+	 */
+	public User findById(long id) {
+		return this.userRepository.findById(id);
+	}
+	
+	/**
+	 * Deletes a user
+	 * @param id
+	 */
 	public void deleteById(long id) {
 		this.userRepository.deleteById(id);
+	}
+
+	/**
+	 * Updates a user
+	 * @param updatedUser
+	 */
+	public void updateUser(long id, User updatedUser) {
+		User user = this.userRepository.findById(id);
+		log.info("User who will be updated: " + user.toString());
+		log.info("Updated with: " + updatedUser.toString());
+		user.setEmail(updatedUser.getEmail());
+		user.setFullname(updatedUser.getFullname());
+		this.userRepository.save(updatedUser);
 	}
 
 }
