0864b074f6c2a16c6192e7d383443eb9caaab532
csabe812 <csafulop@gmail.com>
2019-04-15 20:08:15 +0200

    FEAT|#1|Edited rolerepository for logging in

--- src/main/java/com/agrovetel/repository/RoleRepository.java
+++ src/main/java/com/agrovetel/repository/RoleRepository.java
@@ -4,12 +4,11 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.agrovetel.domain.Role;
-import com.agrovetel.domain.User;
 
 @Repository
 public interface RoleRepository extends CrudRepository<Role, Long> {
 
 	Role findById(long id);
-
 	Role findByRole(String role);
+	
 }
