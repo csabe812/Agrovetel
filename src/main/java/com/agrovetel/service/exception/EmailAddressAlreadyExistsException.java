6b0428470fbb0b664f5c40915123d97a45c3ff95
Csimpi <Csimpi@Csimpi-PC>
2019-04-25 15:44:56 +0200

    Add new Exception(EmailAddressAlreadyExistsException), but not implemented in Service and Controller

---
+++ src/main/java/com/agrovetel/service/exception/EmailAddressAlreadyExistsException.java
@@ -0,0 +1,13 @@
+package com.agrovetel.service.exception;
+
+public class EmailAddressAlreadyExistsException extends Exception {
+	
+	public EmailAddressAlreadyExistsException(String message) {
+		super(message);
+	}
+	
+	public EmailAddressAlreadyExistsException() {
+		
+	}
+
+}
