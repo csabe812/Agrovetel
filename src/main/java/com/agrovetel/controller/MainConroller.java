f396c823352bd6cc2ea992de469119c32672c97c
csabe812 <csafulop@gmail.com>
2019-04-26 18:16:08 +0200

    FEAT|#13|Created a basic site for "mypage"

--- src/main/java/com/agrovetel/controller/MainConroller.java
+++ src/main/java/com/agrovetel/controller/MainConroller.java
@@ -1,21 +1,18 @@
 package com.agrovetel.controller;
+import java.util.Collection;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import com.agrovetel.domain.Ad;
 import com.agrovetel.domain.User;
-import com.agrovetel.service.AdService;
 import com.agrovetel.service.UserService;
 
 @Controller
@@ -42,12 +39,36 @@ public class MainConroller {
 	public String showAds(@ModelAttribute User user) {
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 		log.info("==========");
-		log.info(auth.getAuthorities().toString());
 		log.info("User will be registered");
 		log.info(user.toString());
 		userService.registerUser(user);
 		return "auth/login";
 	}
 	
+	/**
+	 * Deciding where to go: admin site - user site - nowhere
+	 * 
+	 * @return the given site
+	 */
+	@GetMapping("/mysite")
+	public String showMySite() {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		log.info("////");
+		log.info(auth.getName());
+		Collection<? extends GrantedAuthority> authorities = auth.getAuthorities();
+		boolean isGlobalAdmin = authorities.contains(new SimpleGrantedAuthority("GLOBAL_ADMIN"));
+		boolean isLoggedInUser = authorities.contains(new SimpleGrantedAuthority("LOGGED_IN_USER"));
+		boolean isNotLoggedInUser = authorities.contains(new SimpleGrantedAuthority("NOT_LOGGED_IN_USER"));
+		boolean isAdmin = authorities.contains(new SimpleGrantedAuthority("ADMIN"));
+		if(isAdmin || isGlobalAdmin) {
+			return "redirect:/admin/users";
+		}
+		else if(isLoggedInUser) {
+			return "mypage";
+		}
+		else {
+			return "/";
+		}
+	}
 	
 }
